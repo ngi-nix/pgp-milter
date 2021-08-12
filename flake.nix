59dfcf8001ad98bd1ba0ff903554f42e186b1019
Joseph Lucas <lucas.jojo@gmail.com>
2021-08-12 11:37:55 +0200

    split into default+flake, add a devShell and a check

--- flake.nix
+++ flake.nix
@@ -7,9 +7,15 @@
   };
 
   outputs = { self, nixpkgs, pymilter}: let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
-    packages.x86_64-linux.pgp-milter = (import ./default.nix {nixpkgs=nixpkgs; pymilter=pymilter.packages.x86_64-linux.pymilter;});
+    packages.x86_64-linux.pgp-milter = (import ./default.nix {
+      nixpkgs=nixpkgs; 
+      pymilter=pymilter.packages.x86_64-linux.pymilter;
+    });
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.pgp-milter;
-    devShell = pkgs.mkShell { inputsFrom = builtins.attrValues self.system; packages = [ pkgs ]; };
-    meta.longDescription = ''Enables automatic PGP encryption/decryption of e-mails on the server side. It can be used with regular mailservers like postfix. It's basically a milter, that listens for input messages, then looks up PGP keys from configurable sources (local key rings, LDAP) and then, based on a local, configurable, policy, encrypts/decrypts messages (or leaves them untouched) before passing them on. This way system administrators can with tiny effort provide transparent encryption support for all their mail users.'';
+    checks.x86_64-linux.pgp-milter = self.packages.x86_64-linux.pgp-milter;
+    devShell.x86_64-linux = pkgs.mkShell {
+      inputsFrom = builtins.attrValues self.inputs;
+      packages = [ self.packages.x86_64-linux.pgp-milter ];
+    };
   };
 }

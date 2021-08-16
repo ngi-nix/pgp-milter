cae9fd3b825af0dcb71c037570a33e2516681ad0
Joseph Lucas <lucas.jojo@gmail.com>
2021-08-16 11:01:21 +0200

    Remove the x86_64-darwin check

--- flake.nix
+++ flake.nix
@@ -18,7 +18,8 @@
       overlay = final: prev: { pgp-milter = (import ./default.nix { pkgs = final; }); };
       packages = forAllSystems (system: { inherit (nixpkgsFor.${system}) pgp-milter; });
       defaultPackage = forAllSystems (system: self.packages.${system}.pgp-milter);
-      checks = forAllSystems (system: { inherit (self.packages.${system}) pgp-milter; });
+      # FIXME: check also for x86_64-darwin as soon as Hydra will check darwin derivations
+      checks.x86_64-linux.pgp-milter = self.packages.x86_64-linux.pgp-milter;
       devShell = forAllSystems (system: self.packages.${system}.pgp-milter.override { inShell = true; });
   };
 }

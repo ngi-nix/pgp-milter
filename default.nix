4a9fcf699796e8dc35afb8250cfae1d3f5ace1c6
Joseph Lucas <lucas.jojo@gmail.com>
2021-08-16 14:37:06 +0200

    Fix licenses -> license

--- default.nix
+++ default.nix
@@ -19,6 +19,6 @@ with pkgs.python3Packages; buildPythonPackage {
   meta = {
     homepage = https://github.com/ehmry/pgpmilter;
     description = "Enables automatic PGP encryption/decryption of e-mails on the server side. It can be used with regular mailservers like postfix. It's basically a milter, that listens for input messages, then looks up PGP keys from configurable sources (local key rings, LDAP) and then, based on a local, configurable, policy, encrypts/decrypts messages (or leaves them untouched) before passing them on. This way system administrators can with tiny effort provide transparent encryption support for all their mail users.";
-    license = [ pkgs.lib.licenses.agpl3Only ];
+    license = pkgs.lib.licenses.agpl3Only;
   };
 }
